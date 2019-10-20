cdc2555f388d1ada06711a977edd52205acdf875
Karl <afylus@gmail.com>
2019-10-20 19:01:36 -0400

    Updates

---
+++ precache-manifest.4728b4a22d26594454f55280df559369.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a8883247a23fdfa5f51c890ed99e4c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d75f2c4478808d26d834",
+    "url": "/static/css/main.36b4fe17.chunk.css"
+  },
+  {
+    "revision": "8982f4dcc8756c9c7ba5",
+    "url": "/static/js/2.550ec371.chunk.js"
+  },
+  {
+    "revision": "d75f2c4478808d26d834",
+    "url": "/static/js/main.75a19530.chunk.js"
+  },
+  {
+    "revision": "680e3ea1fe534cc03333",
+    "url": "/static/js/runtime-main.2cdcda38.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.32c71fefcc4a4caa2af32d5b101f0537.js"
+  "/precache-manifest.4728b4a22d26594454f55280df559369.js"
 );
 
 self.addEventListener('message', (event) => {
