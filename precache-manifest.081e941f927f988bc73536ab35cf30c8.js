3f6af48cbd1548bb9816d5cc90e83bd247c83f8b
rurisk <afylus@gmail.com>
2020-02-26 17:37:51 -0500

    update to latest

---
+++ precache-manifest.081e941f927f988bc73536ab35cf30c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79acee77ee612be0cabe7df18bcb0b7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b88c4c1a70dc5aac5152",
+    "url": "/static/css/main.b9917bc9.chunk.css"
+  },
+  {
+    "revision": "01c8f1931f90910f0e38",
+    "url": "/static/js/2.64027f01.chunk.js"
+  },
+  {
+    "revision": "b88c4c1a70dc5aac5152",
+    "url": "/static/js/main.cb0f3a05.chunk.js"
+  },
+  {
+    "revision": "680e3ea1fe534cc03333",
+    "url": "/static/js/runtime-main.2cdcda38.js"
+  }
+]);
