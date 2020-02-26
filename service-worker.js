be9e3f30f158b8bbf78c33966721cccd3279e25f
rurisk <afylus@gmail.com>
2020-02-26 17:33:45 -0500

    Revert "Update to latest version."
    This reverts commit 8ad9084f36d374dc8755797129c8edf715952547.

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.081e941f927f988bc73536ab35cf30c8.js"
+  "/precache-manifest.4728b4a22d26594454f55280df559369.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
